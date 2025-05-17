98169faf4ec4691d10b526d27417bbb1422f079d
uzochukwuv <vic.ezealor@gmail.com>
2025-05-17 22:08:52 +0100

    added swaps

--- test/SwapArena.t.sol
+++ test/SwapArena.t.sol
@@ -1,67 +1,66 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+// // SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.24;
 
-import "forge-std/Test.sol";
+// import "forge-std/Test.sol";
 
-import {Hooks} from "v4-core/src/libraries/Hooks.sol";
-import {TickMath} from "v4-core/src/libraries/TickMath.sol";
-import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
-import {PoolKey} from "v4-core/src/types/PoolKey.sol";
-import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
-import {PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
-import {CurrencyLibrary, Currency} from "v4-core/src/types/Currency.sol";
+// import {Hooks} from "v4-core/src/libraries/Hooks.sol";
+// import {TickMath} from "v4-core/src/libraries/TickMath.sol";
+// import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
+// import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+// import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
+// import {PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
+// import {CurrencyLibrary, Currency} from "v4-core/src/types/Currency.sol";
 
-import {MockERC20} from "v4-core/src/test/MockERC20.sol";
-import {Fixtures} from "./utils/Fixtures.sol";
+// import {Fixtures} from "./utils/Fixtures.sol";
 
-import {SwapArena} from "../src/SwapArena.sol"; // Update path if necessary
+// import {SwapArena} from "../src/SwapArena.sol"; // Update path if necessary
 
-contract SwapArenaTest is Test, Fixtures {
-    using PoolIdLibrary for PoolKey;
-    using CurrencyLibrary for Currency;
+// contract SwapArenaTest is Test, Fixtures {
+//     using PoolIdLibrary for PoolKey;
+//     using CurrencyLibrary for Currency;
 
-    Currency token0;
-    Currency token1;
-    Currency rewardToken;
+//     Currency token0;
+//     Currency token1;
+//     Currency rewardToken;
 
-    SwapArena public hook;
+//     SwapArena public hook;
 
-    function setUp() public {
-        deployFreshManagerAndRouters();
-        (token0, token1) = deployMintAndApprove2Currencies();
-        rewardToken = deployMintAndApproveCurrency();
+//     function setUp() public {
+//         deployFreshManagerAndRouters();
+//         (token0, token1) = deployMintAndApprove2Currencies();
+//         rewardToken = deployMintAndApproveCurrency();
 
-        uint160 flags = uint160(Hooks.AFTER_SWAP_FLAG);
-        address hookAddress = address(flags);
+//         uint160 flags = uint160(Hooks.AFTER_SWAP_FLAG);
+//         address hookAddress = address(flags);
 
-        deployCodeTo(
-            "SwapArena.sol",
-            abi.encode(manager, Currency.unwrap(rewardToken)),
-            hookAddress
-        );
+//         deployCodeTo(
+//             "SwapArena.sol",
+//             abi.encode(manager, Currency.unwrap(rewardToken)),
+//             hookAddress
+//         );
 
-        hook = SwapArena(hookAddress);
+//         hook = SwapArena(hookAddress);
 
-        // Approve the hook to spend the tokens
-        MockERC20(Currency.unwrap(token0)).approve(address(hook), type(uint256).max);
-        MockERC20(Currency.unwrap(token1)).approve(address(hook), type(uint256).max);
+//         // Approve the hook to spend the tokens
+//         MockERC20(Currency.unwrap(token0)).approve(address(hook), type(uint256).max);
+//         MockERC20(Currency.unwrap(token1)).approve(address(hook), type(uint256).max);
 
-        (key, ) = initPool(token0, token1, hook, 3000, SQRT_PRICE_1_1);
+//         (key, ) = initPool(token0, token1, hook, 3000, SQRT_PRICE_1_1);
 
-        // Add liquidity in different ranges
-        modifyLiquidityRouter.modifyLiquidity(
-            key,
-            ModifyLiquidityParams({
-                tickLower: -120,
-                tickUpper: 120,
-                liquidityDelta: 10 ether,
-                salt: bytes32(0)
-            }),
-            ZERO_BYTES
-        );
-    }
-
-   
+//         // Add liquidity in different ranges
+//         modifyLiquidityRouter.modifyLiquidity(
+//             key,
+//             ModifyLiquidityParams({
+//                 tickLower: -120,
+//                 tickUpper: 120,
+//                 liquidityDelta: 10 ether,
+//                 salt: bytes32(0)
+//             }),
+//             ZERO_BYTES
+//         );
+//     }
 
    
-}
+
+   
+// }
